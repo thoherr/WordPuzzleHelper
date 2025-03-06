0e53ea6f9d74ff31193ff066bcbdd92f49b6f83e
Thomas Herrmann <mail@thoherr.de>
2025-03-06 15:41:24 +0100

    refactor towards separation of wordlist from checking code

--- main.py
+++ main.py
@@ -3,10 +3,24 @@ import more_itertools
 import sys
 
 
-class WhatMakesSense:
-    def __init__(self, characters):
-        self.characters = list(map(lambda c : c[0], characters))
-        self.wordlist = self._load_wordlist()
+class WordList:
+    def __init__(self, wordlist_filter : lambda x : bool):
+        self.wordlist = self._load_wordlist(wordlist_filter)
+        print(f"Loaded {len(self.wordlist)} words")
+
+    def _load_wordlist(self, wordlist_filter : lambda x : bool):
+        # wordlist from https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4
+        with open('wordlist-german.txt') as f:
+            return list(map(lambda w: w.casefold(),
+                filter(wordlist_filter, f.read().splitlines())))
+
+
+class WhatMakesSense(WordList):
+
+    def __init__(self, characters : list):
+        self.characters = characters
+        l = len(characters)
+        super().__init__(lambda w : len(w) == l)
 
     def meaningful_words(self):
         for sequence in self._generate_permutations():
@@ -17,12 +31,6 @@ class WhatMakesSense:
     def _generate_permutations(self):
         return more_itertools.distinct_permutations(self.characters)
 
-    def _load_wordlist(self):
-        l = len(self.characters)
-        # wordlist from https://gist.github.com/MarvinJWendt/2f4f4154b8ae218600eb091a5706b5f4
-        with open('wordlist-german.txt') as f:
-            return list(map(lambda w: w.casefold(),
-                filter(lambda w : len(w) == l,  f.read().splitlines())))
 
 if __name__ == '__main__':
     whatMakesSense = WhatMakesSense(sys.argv[1:])
